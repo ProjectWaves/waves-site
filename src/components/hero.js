1ea77bd23652f5b6ffe41293e0ed562de10ac4d3
jonathanprozzi <jonathanprozzi@gmail.com>
2019-07-17 10:08:19 -0400

    removed animated chevron svg and associated code from the hero component

--- src/components/hero.js
+++ src/components/hero.js
@@ -10,23 +10,23 @@ import { DialogContent, DialogOverlay } from '@reach/dialog';
 import heroImg from '../images/waves-hero.jpg';
 import { redirectTo } from '@reach/router';
 
-const pulsing = keyframes`
- from, 20%, 53%, 80%, to {
-    transform: translate3d(0,0,0);
-  }
+// const pulsing = keyframes`
+//  from, 20%, 53%, 80%, to {
+//     transform: translate3d(0,0,0);
+//   }
 
-  40%, 43% {
-    transform: translate3d(0, -10px, 0);
-  }
+//   40%, 43% {
+//     transform: translate3d(0, -10px, 0);
+//   }
 
-  70% {
-    transform: translate3d(0, -6px, 0);
-  }
+//   70% {
+//     transform: translate3d(0, -6px, 0);
+//   }
 
-  90% {
-    transform: translate3d(0,-3px,0);
-  }
-`;
+//   90% {
+//     transform: translate3d(0,-3px,0);
+//   }
+// `;
 
 const Hero = () => {
   const [showLightbox, setShowLightbox] = useState(false);
@@ -150,7 +150,7 @@ const Hero = () => {
             Join the Network
           </PrimaryCTAButton>
         </CTAContainer>
-        <VisuallyHidden>Scroll Down</VisuallyHidden>
+        {/* <VisuallyHidden>Scroll Down</VisuallyHidden>
         <animated.svg
           css={css`
             position: absolute;
@@ -171,7 +171,7 @@ const Hero = () => {
             d="M119.851 80.6841C121.359 79.5527 123.225 79.0035 125.106 79.1371C126.987 79.2708 128.756 80.0784 130.09 81.4118C131.423 82.7451 132.23 84.5146 132.364 86.3955C132.498 88.2763 131.949 90.1423 130.817 91.6508L99.4839 122.984C98.0196 124.419 96.0509 125.223 94.0005 125.223C91.9501 125.223 89.9815 124.419 88.5172 122.984L57.1839 91.6508C56.0525 90.1423 55.5032 88.2763 55.6369 86.3955C55.7706 84.5146 56.5782 82.7451 57.9115 81.4118C59.2449 80.0784 61.0144 79.2708 62.8952 79.1371C64.7761 79.0035 66.642 79.5527 68.1505 80.6841L94.0005 106.456L119.851 80.6058V80.6841Z"
             fill="white"
           />
-        </animated.svg>
+        </animated.svg> */}
       </Masthead>
       {showLightbox && (
         <StyledDialogOverlay
