763cb5d28e940df012846a933fd414cebf1b9861
jonathanprozzi <jonathanprozzi@gmail.com>
2019-08-03 18:37:40 -0400

    testing form progress thus far

--- src/components/join-network-form.js
+++ src/components/join-network-form.js
@@ -4,45 +4,65 @@ import { css } from '@emotion/core';
 const JoinNetworkForm = () => (
   <form
     method="post"
-    name="waves-gatsby-code"
+    name="wavesInterestForm"
     netlify-honeypot="bot-field"
     data-netlify="true"
     css={css`
-      background: tomato;
+      max-width: 600px;
+      margin: 2rem auto;
+      label {
+        font-size: 1.5rem;
+        margin-bottom: 0.5rem;
+      }
+      input,
+      textarea {
+        font-size: 1.5rem;
+        border: 2px solid rgba(0, 0, 0, 0.2);
+        border-radius: 0.5rem;
+        padding: 0.5rem;
+        margin-bottom: 1.5rem;
+      }
+      input::placeholder,
+      textarea::placeholder {
+        font-size: 1.5rem;
+        color: #777;
+      }
     `}
   >
-    <input
+    <div
       css={css`
-        color: red;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
       `}
-      type="hidden"
-      name="bot-field"
-    />
-    <label>
-      Name
-      <input
-        css={css`
-          color: red;
-        `}
-        type="text"
-        name="name"
-        id="name"
-      />
-    </label>
-    <label>
-      Email
-      <input type="email" name="email" id="email" />
-    </label>
-    <label>
-      Subject
-      <input type="text" name="subject" id="subject" />
-    </label>
-    <label>
-      Message
-      <textarea name="message" id="message" rows="5" />
-    </label>
-    <button type="submit">Send</button>
-    <input type="reset" value="Clear" />
+    >
+      <input type="hidden" name="bot-field" />
+      <label>
+        Name
+        <input
+          css={css`
+            color: red;
+          `}
+          type="text"
+          name="name"
+          id="name"
+        />
+      </label>
+      <label>
+        Phone
+        <input type="tel" name="phone" id="phone" />
+      </label>
+      <label>
+        Address
+        <textarea name="address" id="address" rows="5" />
+      </label>
+      <label>
+        Email
+        <input type="email" name="email" id="email" />
+      </label>
+      <button type="submit">Send</button>
+      <input type="reset" value="Clear" />
+    </div>
   </form>
 );
 
