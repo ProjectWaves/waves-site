3401a8b3948be59b2234f464d9d3478d34436e8c
jonathanprozzi <jonathanprozzi@gmail.com>
2019-08-05 16:58:40 -0400

    removed unused components from JoinTheNetwork page

--- src/pages/join-the-network.js
+++ src/pages/join-the-network.js
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'gatsby';
-import styled from '@emotion/styled';
 import { css, jsx } from '@emotion/core';
 import { Waypoint } from 'react-waypoint';
 import { useSpring, animated, config } from 'react-spring';
 import Layout from '../components/layout';
-import { AppWrapper } from '../components/elements/layout-components';
 import Header from '../components/header';
 import InfoFooter from '../components/info-footer';
 import Footer from '../components/footer';
