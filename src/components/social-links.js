54289b4fec795d04274c91ab63c299d2995d16a8
jonathanprozzi <jonathanprozzi@gmail.com>
2019-09-04 12:55:28 -0400

    updated social-links to use react-icons and icon context

--- src/components/social-links.js
+++ src/components/social-links.js
@@ -1,60 +1,77 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import Icon from './elements/icon';
+import {
+  FaGithub,
+  FaFacebookF,
+  FaTwitter,
+  FaYoutube,
+  FaFlickr,
+  FaInstagram,
+} from 'react-icons/fa';
+import { IconContext } from 'react-icons';
 
 const SocialLinks = () => (
-  <LinkContainer>
+  <IconContext.Provider value={{ color: 'var(--primary-color)', size: '24px' }}>
+    <LinkContainer>
       <a
-      href="https://github.com/ProjectWaves"
-      aria-label="Find Waves on GitHub"
-      target="_blank"
-      rel="nofollow noopener"
-    >
-      <Icon color="var(--primary-color)" type="github" />
-    </a>
+        href="https://github.com/ProjectWaves"
+        aria-label="Find Waves on GitHub"
+        target="_blank"
+        rel="nofollow noopener"
+      >
+        {/* <Icon color="var(--primary-color)" type="github" /> */}
+        <FaGithub />
+      </a>
 
-    <a
-      href="https://www.facebook.com/DigitalHarbor"
-      aria-label="Find Digital Harbor Foundation on Facebook"
-      target="_blank"
-      rel="nofollow noopener"
-    >
-      <Icon color="var(--primary-color)" type="facebook" />
-    </a>
+      <a
+        href="https://www.facebook.com/DigitalHarbor"
+        aria-label="Find Digital Harbor Foundation on Facebook"
+        target="_blank"
+        rel="nofollow noopener"
+      >
+        {/* <Icon color="var(--primary-color)" type="facebook" /> */}
+        <FaFacebookF />
+      </a>
 
-    <a
-      href="https://twitter.com/WavesMesh"
-      aria-label="Find Waves on Twitter"
-      target="_blank"
-      rel="nofollow noopener"
-    >
-      <Icon color="var(--primary-color)" type="twitter" />
-    </a>
-    <a
-      href="https://www.youtube.com/user/DHFBaltimore"
-      aria-label="Find Digital Harbor Foundation on YouTube"
-      target="_blank"
-      rel="nofollow noopener"
-    >
-      <Icon color="var(--primary-color)" type="youtube" />
-    </a>
-    <a
-      href="https://www.flickr.com/photos/digitalharbor/"
-      aria-label="Find Digital Harbor Foundation on Flickr"
-      target="_blank"
-      rel="nofollow noopener"
-    >
-      <Icon color="var(--primary-color)" type="flickr" />
-    </a>
-    <a
-      href="https://www.instagram.com/dhfbaltimore/"
-      aria-label="Find Digital Harbor Foundation on Instagram"
-      target="_blank"
-      rel="nofollow noopener"
-    >
-      <Icon color="var(--primary-color)" type="instagram" />
-    </a>
-  </LinkContainer>
+      <a
+        href="https://twitter.com/WavesMesh"
+        aria-label="Find Waves on Twitter"
+        target="_blank"
+        rel="nofollow noopener"
+      >
+        {/* <Icon color="var(--primary-color)" type="twitter" /> */}
+        <FaTwitter></FaTwitter>
+      </a>
+      <a
+        href="https://www.youtube.com/user/DHFBaltimore"
+        aria-label="Find Digital Harbor Foundation on YouTube"
+        target="_blank"
+        rel="nofollow noopener"
+      >
+        {/* <Icon color="var(--primary-color)" type="youtube" /> */}
+        <FaYoutube></FaYoutube>
+      </a>
+      <a
+        href="https://www.flickr.com/photos/digitalharbor/"
+        aria-label="Find Digital Harbor Foundation on Flickr"
+        target="_blank"
+        rel="nofollow noopener"
+      >
+        {/* <Icon color="var(--primary-color)" type="flickr" /> */}
+        <FaFlickr></FaFlickr>
+      </a>
+      <a
+        href="https://www.instagram.com/dhfbaltimore/"
+        aria-label="Find Digital Harbor Foundation on Instagram"
+        target="_blank"
+        rel="nofollow noopener"
+      >
+        {/* <Icon color="var(--primary-color)" type="instagram" /> */}
+        <FaInstagram></FaInstagram>
+      </a>
+    </LinkContainer>
+  </IconContext.Provider>
 );
 
 export default SocialLinks;
