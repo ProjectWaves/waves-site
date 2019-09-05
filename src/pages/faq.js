9bee4a55ab406d7d3ae34404cac17aca2fe1bc82
jonathanprozzi <jonathanprozzi@gmail.com>
2019-09-05 16:24:44 -0400

    committing skeleton of faq.js

---
+++ src/pages/faq.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import { css } from '@emotion/core';
+import { ContentWrapper } from '../components/elements/layout-components';
+import Layout from '../components/layout';
+import Header from '../components/header';
+import InfoFooter from '../components/info-footer';
+import Footer from '../components/footer';
+
+const FAQPage = () => (
+  <Layout>
+    <Header />
+    <ContentWrapper>
+      <h2
+        css={css`
+          font-size: 4rem;
+          color: #121212;
+        `}
+      >
+        Frequently Asked Questions
+      </h2>
+      <h3>
+        1. What is your product (Internet), what speeds will you provide (and
+        how does that compare to your competition){' '}
+      </h3>
+      <p>
+        The goal when connecting any individual is to make sure they're getting
+        high-speed internet(25mb+) with no data cap, and no throttling. I do not
+        necessarily see this as competition, but rather as a way for Baltimore
+        to circumvent ISPs like Comcast which tend to price gouge consumers
+        while also delivering a sub-par service. The recommended tax-deductible
+        donation amount is $10 a month for service, $120 for installation.
+        Monthly donation & installation fees, however, are Pay-What-You-Can, as
+        I strongly believe that pricing should not be a barrier to internet
+        access.{' '}
+      </p>
+    </ContentWrapper>
+    <InfoFooter />
+    <Footer />
+  </Layout>
+);
+
+export default FAQPage;
